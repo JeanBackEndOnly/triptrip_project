6634233fa74118b40bcef0d2c35de17a1a2d4f09
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2025-09-17 14:10:47 +0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php 
+include 'authentication/functions.php';
+include 'authentication/session.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="<?php echo get_option('system_description')?>">
+    <title>TEMPLATE</title>
+    <?php render_styles()?>
+    <!-- <link rel="stylesheet" href="../assets/csss/users.css"> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script>
+        var base_url = '<?php echo base_url() ?>';
+    </script>
+    <?php render_scripts() ?>
+</head>
+    <body class="bg-light-300">
